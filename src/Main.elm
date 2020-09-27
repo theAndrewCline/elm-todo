a4eede78f50f4b6c0605e51d46596be3d886e429
Andrew Cline <andrew.cline77@gmail.com>
2020-09-27 08:22:57 -0500

    add todo addition and completion

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,38 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (button, div, p, text)
-import Html.Attributes exposing (class)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, input, p, text)
+import Html.Attributes exposing (checked, class, placeholder, type_, value)
+import Html.Events exposing (onClick, onInput)
+
+
+
+-- PROGRAM
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init =
+            { list =
+                [ { title = "Code More Elm!"
+                  , completed = False
+                  , id = 1
+                  }
+                , { title = "Make More Things"
+                  , completed = False
+                  , id = 2
+                  }
+                ]
+            , input = ""
+            }
+        , view = view
+        , update = update
+        }
+
+
+
+-- MODEL
 
 
 type alias Todo =
@@ -19,58 +48,94 @@ type alias Model =
     }
 
 
+
+-- UPDATE
+
+
 type Msg
-    = AddTodo String
-    | CompleteTodo Int
+    = AddTodo
+    | ToggleCompleted Int
     | DeleteTodo Int
+    | UpdateInput String
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        AddTodo title ->
+        AddTodo ->
+            { input = ""
+            , list =
+                model.list
+                    ++ [ { title = model.input
+                         , completed = False
+                         , id = 1
+                         }
+                       ]
+            }
+
+        UpdateInput string ->
+            { model | input = string }
+
+        ToggleCompleted id ->
             { model
                 | list =
-                    model.list
-                        ++ [ { title = title
-                             , completed = False
-                             , id = 1
-                             }
-                           ]
+                    List.map
+                        (\x ->
+                            if x.id == id then
+                                { x | completed = not x.completed }
+
+                            else
+                                x
+                        )
+                        model.list
             }
 
         _ ->
             model
 
 
-view : Model -> Html.Html Msg
+
+-- VIEW
+
+
+view : Model -> Html Msg
 view model =
     div [ class "bg-gray-200 min-h-screen pt-8" ]
         [ div
             [ class "container mx-auto" ]
             [ div
-                [ class "bg-white shadow-lg rounded w-1/3 mx-auto flex flex-col items-center justify-center" ]
-                [ div
-                    []
-                  <|
-                    List.map
-                        (\todo -> p [ class "text-4xl" ] [ text todo.title ])
-                        model.list
-                , div []
-                    [ button [ class "bg-blue-500 px-8 py-2 m-2 rounded", onClick <| AddTodo "Code More Elm" ] [ text "+" ]
-                    ]
+                [ class "bg-white shadow-lg rounded w-2/3 mx-auto flex flex-col items-left justify-center" ]
+                [ listView model.list
+                , inputView model.input
                 ]
             ]
         ]
 
 
-main : Program () Model Msg
-main =
-    Browser.sandbox
-        { init =
-            { list = []
-            , input = ""
-            }
-        , view = view
-        , update = update
-        }
+inputView : String -> Html Msg
+inputView v =
+    div [ class "m-auto" ]
+        [ input [ class "border px-4 py-2 m-2 text-xl", onInput UpdateInput, value v, placeholder "Add a todo!" ] []
+        , button [ class "bg-blue-400 px-8 py-2 m-2 rounded text-xl text-black", onClick AddTodo ] [ text "+" ]
+        ]
+
+
+listView : List Todo -> Html Msg
+listView list =
+    div [] <| List.map todoView list
+
+
+todoView : Todo -> Html Msg
+todoView todo =
+    let
+        classes =
+            if todo.completed then
+                "ml-2 text-xl line-through text-gray-500"
+
+            else
+                "ml-2 text-xl"
+    in
+    div [ class "flex items-center mx-4", onClick (ToggleCompleted todo.id) ]
+        [ input [ type_ "checkbox", checked todo.completed ] []
+        , p [ class classes ] [ text todo.title ]
+        ]
